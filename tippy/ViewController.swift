4b1a4dd59198248c815e1697c453b199c000db78
Duy Le <leduy10192@gmail.com>
2019-12-19 12:35:45 -0800

    first commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Duy Le on 12/19/19.
+//  Copyright © 2019 Duy Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
